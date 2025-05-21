ee758c98ee5c8532e9d14ce470584061286bfdbb
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-21 21:26:11 +0800

    refactor: Move generatePlayField to Playfield class

--- src/main/java/com/mjfelecio/beatsync/Main.java
+++ src/main/java/com/mjfelecio/beatsync/Main.java
@@ -1,6 +1,7 @@
 package com.mjfelecio.beatsync;
 
 import com.mjfelecio.beatsync.core.Playfield;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -12,16 +13,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
     private final int WIDTH = 1920;
     private final int HEIGHT = 1080;
-
+    private final int PLAYFIELD_WIDTH = 400;
+    private final int PLAYFIELD_HEIGHT = 700;
     private Playfield playfield;
 
     @Override
     public void start(Stage stage) {
+        Canvas canvas = new Canvas(PLAYFIELD_WIDTH, PLAYFIELD_HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        playfield = new Playfield(PLAYFIELD_WIDTH, PLAYFIELD_HEIGHT);
+        playfield.render(gc);
 
-        Canvas playField = generatePlayField();
-
-        Scene scene = new Scene(new StackPane(playField), WIDTH, HEIGHT);
+        Scene scene = new Scene(new StackPane(canvas), WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.setTitle("Beat Sync: VSRG made with Java");
         stage.show();
@@ -30,41 +34,10 @@ public class Main extends Application {
 //            @Override
 //            public void handle(long now) {
 //                gc.clearRect(0, 0, WIDTH, HEIGHT);
-//                playField.render(gc);
-//                // In future: noteManager.renderNotes(gc, audioTime);
+//
+//                playfield.render(gc);
 //            }
 //        }.start();
-    }
-
-    private Canvas generatePlayField() {
-        final int PLAY_FIELD_HEIGHT = 700;
-        final int PLAY_FIELD_WIDTH = 400;
-        final int NUM_OF_LANES = 4;
-        final int LANE_WIDTH = PLAY_FIELD_WIDTH / NUM_OF_LANES;
-        final int NOTE_SIZE = 80;
-        final int HIT_ZONE_HEIGHT = PLAY_FIELD_HEIGHT - 150;
-
-        Canvas playField = new Canvas(PLAY_FIELD_WIDTH, PLAY_FIELD_HEIGHT);
-        GraphicsContext gc = playField.getGraphicsContext2D();
-
-        // Create border
-        gc.setFill(Color.BLACK);
-        gc.setLineWidth(8);
-        gc.strokeRect(0, 0 , PLAY_FIELD_WIDTH, PLAY_FIELD_HEIGHT);
-
-        for (int i = 0; i < NUM_OF_LANES; i++) {
-            gc.setLineWidth(2);
-
-            // Add vertical lines as lane separators
-            gc.strokeLine(LANE_WIDTH * i, 0, LANE_WIDTH * i, PLAY_FIELD_HEIGHT);
-
-            int circleCenteredWidthPos = (LANE_WIDTH * i) + (LANE_WIDTH - NOTE_SIZE) / 2;
-
-            // Add circles as indications for the hit zones in each lane
-            gc.strokeOval(circleCenteredWidthPos, HIT_ZONE_HEIGHT, NOTE_SIZE, NOTE_SIZE);
-        }
-
-        return playField;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/mjfelecio/beatsync/core/Playfield.java
+++ src/main/java/com/mjfelecio/beatsync/core/Playfield.java
@@ -1,9 +1,46 @@
 package com.mjfelecio.beatsync.core;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Playfield {
+    private int width;
+    private int height;
+    private final int LANES = 4;
+    private final int NOTE_SIZE = 80;
 
-    public Playfield() {
+    public Playfield(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 
+    public void render(GraphicsContext gc) {
+        int laneWidth = getLaneWidth();
+        // Create border
+        gc.setFill(Color.BLACK);
+        gc.setLineWidth(8);
+        gc.strokeRect(0, 0, width, height);
+
+        for (int i = 0; i < LANES; i++) {
+            gc.setLineWidth(2);
+
+            // Add vertical lines as lane separators
+            gc.strokeLine(laneWidth * i, 0, laneWidth * i, height);
+
+            int circleCenteredWidthPos = (laneWidth * i) + (laneWidth - NOTE_SIZE) / 2;
+            int hitZonePos = getHitZonePos();
+
+            // Add circles as indications for the hit zones in each lane
+            gc.strokeOval(circleCenteredWidthPos, hitZonePos, NOTE_SIZE, NOTE_SIZE);
+        }
+    }
+
+    private int getLaneWidth() {
+        return width / LANES;
+    }
+
+    private int getHitZonePos() {
+        return height - 150;
     }
 
 
