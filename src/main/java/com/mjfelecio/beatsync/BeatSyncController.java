084a36eb0f2c050527b7601cde933094d530e1af
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-21 15:28:46 +0800

    refactor: Rename files

--- src/main/java/com/mjfelecio/beatsync/BeatSyncController.java
+++ src/main/java/com/mjfelecio/beatsync/BeatSyncController.java
@@ -2,12 +2,8 @@ package com.mjfelecio.beatsync;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import org.hydev.obp.Beatmap;
-import org.hydev.obp.BeatmapReader;
 
-import java.io.File;
-
-public class HelloController {
+public class BeatSyncController {
 
     @FXML
     private Label welcomeText;
