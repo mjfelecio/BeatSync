e14202927eade39ca4387a4fd29b58d064f97cb3
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-22 19:37:05 +0800

    feat: Create BeatmapParser class
    This class handles converting a beatmap file into something that the program can use.

---
+++ src/main/java/com/mjfelecio/beatsync/core/BeatmapParser.java
@@ -0,0 +1,29 @@
+package com.mjfelecio.beatsync.core;
+
+import org.hydev.obp.Beatmap;
+
+import java.util.ArrayList;
+
+public class BeatmapParser {
+    private Beatmap beatmap;
+
+    public BeatmapParser(Beatmap beatmap) {
+        this.beatmap = beatmap;
+    }
+
+    public void parse() {
+    }
+
+    public ArrayList<Note> parseNotes() {
+        ArrayList<Note> notes = new ArrayList<>();
+
+        System.out.println(this.beatmap.getObjects());
+
+        this.beatmap.getObjects().forEach(h -> {
+            notes.add(new Note(h.getX(), h.getTime()));
+        });
+
+        System.out.println(notes);
+        return notes;
+    }
+}
