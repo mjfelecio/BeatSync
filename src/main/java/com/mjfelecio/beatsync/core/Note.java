cf58ab74ff1c0a91b681d52a9d44e0379663e300
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-22 16:15:32 +0800

    feat: Add Note class to represent note

---
+++ src/main/java/com/mjfelecio/beatsync/core/Note.java
@@ -0,0 +1,20 @@
+package com.mjfelecio.beatsync.core;
+
+public class Note {
+    private final int x;
+    private int y = -10; // Notes start at the very top of the screen anyway
+    private final int startTime;
+
+    public Note(int x, int startTime) {
+        this.x = x;
+        this.startTime = startTime;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getStartTime() { return startTime; }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
