627596efceec3249a0bd2e48150bc5f00b594e4d
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-25 18:48:52 +0800

    feat: Implement drawLanes() in PlayfieldRenderer

--- src/main/java/com/mjfelecio/beatsync/rendering/PlayfieldRenderer.java
+++ src/main/java/com/mjfelecio/beatsync/rendering/PlayfieldRenderer.java
@@ -1,13 +1,23 @@
 package com.mjfelecio.beatsync.rendering;
 
 import com.mjfelecio.beatsync.config.GameConfig;
+import com.mjfelecio.beatsync.core.GameState;
+import com.mjfelecio.beatsync.core.Note;
+import com.mjfelecio.beatsync.input.InputState;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class PlayfieldRenderer {
-    public void render(GraphicsContext gc) {
+    public void render(GraphicsContext gc, GameState gameState,
+                       List<Note> visibleNotes, InputState inputState) {
+
         clearScreen(gc);
         drawPlayfieldBorders(gc);
+        drawLanes(gc, inputState);
+//        drawNotes(gc, visibleNotes);
+//        drawUI(gc, gameState);
     }
 
     public void clearScreen(GraphicsContext gc) {
@@ -20,4 +30,26 @@ public class PlayfieldRenderer {
         gc.strokeRect(0, 0, GameConfig.PLAYFIELD_WIDTH, GameConfig.PLAYFIELD_HEIGHT);
     }
 
+    public void drawLanes(GraphicsContext gc, InputState inputState) {
+        int laneWidth = GameConfig.PLAYFIELD_WIDTH / GameConfig.NUM_LANES;
+
+        for (int i = 0; i < GameConfig.NUM_LANES; i++) {
+            // Draw lane separator
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(2);
+            gc.strokeLine(laneWidth * i, 0, laneWidth * i, GameConfig.PLAYFIELD_HEIGHT);
+
+            // Draw hit zone indicators
+            int centerX = (laneWidth * i) + (laneWidth - GameConfig.NOTE_DIAMETER) / 2;
+            gc.strokeOval(centerX, GameConfig.HIT_LINE_Y, GameConfig.NOTE_DIAMETER, GameConfig.NOTE_DIAMETER);
+
+            if (inputState.isLanePressed(i)) {
+                gc.setFill(Color.RED);
+                gc.fillOval(centerX, GameConfig.HIT_LINE_Y, GameConfig.NOTE_DIAMETER, GameConfig.NOTE_DIAMETER);
+                gc.setFill(Color.BLACK);
+            }
+
+            gc.strokeOval(centerX, GameConfig.HIT_LINE_Y, GameConfig.NOTE_DIAMETER, GameConfig.NOTE_DIAMETER);
+        }
+    }
 }
