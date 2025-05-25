2dbe3eb33353fe671cc1a6a1a280039b4595f9b4
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-25 15:10:55 +0800

    feat: Create InputManager

---
+++ src/main/java/com/mjfelecio/beatsync/InputManager.java
@@ -0,0 +1,34 @@
+package com.mjfelecio.beatsync;
+
+import com.mjfelecio.beatsync.core.Constants;
+import javafx.scene.input.KeyCode;
+
+public class InputManager {
+    private boolean[] lanePressed = new boolean[Constants.NUM_LANES];
+
+    public void press(KeyCode code) {
+        switch (code) {
+            case D -> setLanePressed(0, true);
+            case F -> setLanePressed(1, true);
+            case J -> setLanePressed(2, true);
+            case K -> setLanePressed(3, true);
+        }
+    }
+
+    public void release(KeyCode code) {
+        switch (code) {
+            case D -> setLanePressed(0, false);
+            case F -> setLanePressed(1, false);
+            case J -> setLanePressed(2, false);
+            case K -> setLanePressed(3, false);
+        }
+    }
+
+    public void setLanePressed(int lane, boolean state) {
+        this.lanePressed[lane] = state;
+    }
+
+    public boolean isPressed(int lane) {
+        return lanePressed[lane];
+    }
+}
