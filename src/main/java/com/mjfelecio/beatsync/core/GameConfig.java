8f6bd7d5135df771e9332be0eca1b8444ce0bc51
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-25 15:58:42 +0800

    feat: Add screen size constants to GameConfig

--- src/main/java/com/mjfelecio/beatsync/core/GameConfig.java
+++ src/main/java/com/mjfelecio/beatsync/core/GameConfig.java
@@ -1,12 +1,15 @@
 package com.mjfelecio.beatsync.core;
 
 public final class GameConfig {
-    public static final int NUM_LANES = 4;
-    public static final int NOTE_DIAMETER = 80;
-    public static final int NOTE_APPROACH_TIME = 1000;
-
+    public static final int SCREEN_WIDTH = 1920;
+    public static final int SCREEN_HEIGHT = 1080;
     public static final int PLAYFIELD_WIDTH = 400;
     public static final int PLAYFIELD_HEIGHT = 700;
+
+    public static final int NUM_LANES = 4;
+    public static final int NOTE_DIAMETER = 80;
+    // Will probably swap this to a double later if I want them to control approach time
+    public static final int NOTE_APPROACH_TIME = 1000;
 
     public static final int HIT_LINE_Y = PLAYFIELD_HEIGHT - 150;
     public static final String TEST_BEATMAP_PATH = "src/main/resources/com/mjfelecio/beatsync/beatmaps/test.osu";
