7faab98594109b840cbf240d48d8971798f3971f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-25 10:17:52 +0800

    feat: Repurpose BeatmapParser to BeatmapLoader
    Instead of being a duplicate parser, this now handles
    the loading of the beatmap itself so that it is usable
    directly, including connecting it to its corresponding
    audio file. I haven't implemented it's future yet though.

---
+++ src/main/java/com/mjfelecio/beatsync/core/BeatmapLoader.java
@@ -0,0 +1,19 @@
+package com.mjfelecio.beatsync.core;
+
+import com.mjfelecio.beatsync.parser.obj.Beatmap;
+
+public class BeatmapLoader {
+    private Beatmap beatmap;
+
+    public BeatmapLoader(Beatmap beatmap) {
+        this.beatmap = beatmap;
+    }
+
+    public static void load() {
+
+    }
+
+
+
+
+}
