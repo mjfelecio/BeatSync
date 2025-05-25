b9bcf88fbb656f794047463832f5b4f2b53c1c6d
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-25 16:43:55 +0800

    feat: Implement clearScreen() and drawPlayfieldBorders in PlayfieldRenderer

--- src/main/java/com/mjfelecio/beatsync/rendering/PlayfieldRenderer.java
+++ src/main/java/com/mjfelecio/beatsync/rendering/PlayfieldRenderer.java
@@ -1,4 +1,23 @@
 package com.mjfelecio.beatsync.rendering;
 
+import com.mjfelecio.beatsync.config.GameConfig;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class PlayfieldRenderer {
+    public void render(GraphicsContext gc) {
+        clearScreen(gc);
+        drawPlayfieldBorders(gc);
+    }
+
+    public void clearScreen(GraphicsContext gc) {
+        gc.clearRect(0, 0, GameConfig.PLAYFIELD_WIDTH, GameConfig.PLAYFIELD_HEIGHT);
+    }
+
+    public void drawPlayfieldBorders(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.setLineWidth(8);
+        gc.strokeRect(0, 0, GameConfig.PLAYFIELD_WIDTH, GameConfig.PLAYFIELD_HEIGHT);
+    }
+
 }
