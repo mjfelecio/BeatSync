32bf62cf48f9df5e1c30d0a576ed92a9f25cb86a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-27 07:43:23 +0800

    feat: judge note in JudgementProcessor using deltaTime
    This is genius I say hahahhahah, very simple

--- src/main/java/com/mjfelecio/beatsync/judgement/JudgementProcessor.java
+++ src/main/java/com/mjfelecio/beatsync/judgement/JudgementProcessor.java
@@ -4,16 +4,14 @@ import com.mjfelecio.beatsync.core.Note;
 
 public class JudgementProcessor {
 
-    public static JudgementResult judge(Note n, long timeElapsed) {
-        JudgementResult judgementResult = JudgementResult.NONE;
-        if (isMiss(n, timeElapsed)) {
-            judgementResult = JudgementResult.MISS;
-        }
+    public static JudgementResult judge(Note n, long currentTime) {
+        JudgementResult judgementResult;
+        long deltaTime = Math.abs(currentTime - n.getStartTime());
+
+        if (deltaTime <= JudgementWindow.PERFECT.getMillis()) judgementResult = JudgementResult.PERFECT;
+        else if (deltaTime <= JudgementWindow.GOOD.getMillis()) judgementResult = JudgementResult.GOOD ;
+        else judgementResult = JudgementResult.MISS;
 
         return judgementResult;
-    }
-
-    private static boolean isMiss(Note n, long timeElapsed) {
-        return !n.isHit() && (timeElapsed - n.getStartTime()) > JudgementWindow.MISS.getMillis();
     }
 }
