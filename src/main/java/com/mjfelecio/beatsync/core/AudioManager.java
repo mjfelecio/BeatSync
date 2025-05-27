4861fa7274d45f828e3a7eaa6fc929d91bff891e
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-27 17:10:33 +0800

    feat: Add new methods in AudioManager for better control

--- src/main/java/com/mjfelecio/beatsync/core/AudioManager.java
+++ src/main/java/com/mjfelecio/beatsync/core/AudioManager.java
@@ -4,15 +4,55 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class AudioManager {
-    private Media music;
-    private MediaPlayer mediaPlayer;
+    private Media currentMusic;
+    private MediaPlayer player;
 
-    public void setMusic(Media newMusic) {
-        this.music = newMusic;
-        this.mediaPlayer = new MediaPlayer(this.music);
+    public void setCurrentMusic(Media newMusic) {
+        this.currentMusic = newMusic;
+        this.player = new MediaPlayer(this.currentMusic);
     }
 
     public MediaPlayer getPlayer() {
-        return mediaPlayer;
+        return player;
+    }
+
+    public void loadMusic(String audioPath) {
+        Media media = new Media(audioPath);
+        setCurrentMusic(media);
+    }
+
+    public void play() {
+        if (player != null) {
+            player.play();
+        }
+    }
+
+    public void pause() {
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    public void stop() {
+        if (player != null) {
+            player.stop();
+        }
+    }
+
+    public long getCurrentTime() {
+        if (player != null && player.getCurrentTime() != null) {
+            return (long) player.getCurrentTime().toMillis();
+        }
+        return 0;
+    }
+
+    public boolean isPlaying() {
+        return player != null && player.getStatus() == MediaPlayer.Status.PLAYING;
+    }
+
+    public void dispose() {
+        if (player != null) {
+            player.dispose();
+        }
     }
 }
