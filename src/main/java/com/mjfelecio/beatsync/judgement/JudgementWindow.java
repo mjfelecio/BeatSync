00310b21a258c34513fe67e7f4aaeab837b3529f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-29 20:26:22 +0800

    feat: Add tail judgement windows
    For some reason, the judgement windows when judging the tail is too small
    to be playable. Hence, I doubled it compared to normal note window.

--- src/main/java/com/mjfelecio/beatsync/judgement/JudgementWindow.java
+++ src/main/java/com/mjfelecio/beatsync/judgement/JudgementWindow.java
@@ -3,7 +3,10 @@ package com.mjfelecio.beatsync.judgement;
 public enum JudgementWindow {
     PERFECT(30, "Perfect"),
     GOOD(80, "Good"),
-    MISS(150, "Miss");
+    MISS(150, "Miss"),
+    PERFECT_TAIL(60, "Perfect"),
+    GOOD_TAIL(160, "Good"),
+    MISS_TAIL(300, "Miss");
 
     private final int millis;
     private final String description;
