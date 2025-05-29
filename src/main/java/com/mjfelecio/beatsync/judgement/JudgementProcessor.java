40863209ff65c8c95286dd70cd31c745d18b4a41
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-29 09:51:23 +0800

    feat: Pass noteTime in JudgementProcessor instead of the note itself
    This makes it possible to reuse it for judging normal notes
    and judging the release of a hold note

--- src/main/java/com/mjfelecio/beatsync/judgement/JudgementProcessor.java
+++ src/main/java/com/mjfelecio/beatsync/judgement/JudgementProcessor.java
@@ -4,9 +4,13 @@ import com.mjfelecio.beatsync.object.Note;
 
 public class JudgementProcessor {
 
-    public static JudgementResult judge(Note n, long currentTime) {
+    // Judge the press of a note based on the noteTime and the current time
+    // If we want to judge a normal note, we pass the startTime as the noteTime
+    // If we want to judge a hold note, we pass the endTime instead,
+    // so we can judge when it was released
+    public static JudgementResult judge(long noteTime, long currentTime) {
         JudgementResult judgementResult;
-        long deltaTime = Math.abs(currentTime - n.getStartTime());
+        long deltaTime = Math.abs(currentTime - noteTime);
 
         if (deltaTime <= JudgementWindow.PERFECT.getMillis()) judgementResult = JudgementResult.PERFECT;
         else if (deltaTime <= JudgementWindow.GOOD.getMillis()) judgementResult = JudgementResult.GOOD ;
