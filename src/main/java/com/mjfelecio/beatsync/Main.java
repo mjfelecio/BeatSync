afe9a9285f917fb67e177bcb9b88a8819c0549e1
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-05-31 17:22:05 +0800

    feat: Use the new approach in Main (duplicate commit I know)

--- src/main/java/com/mjfelecio/beatsync/Main.java
+++ src/main/java/com/mjfelecio/beatsync/Main.java
@@ -1,6 +1,8 @@
 package com.mjfelecio.beatsync;
 
 import com.mjfelecio.beatsync.config.GameConfig;
+import com.mjfelecio.beatsync.core.GameEngine;
+import com.mjfelecio.beatsync.core.GameplayManager;
 import com.mjfelecio.beatsync.state.GameState;
 import com.mjfelecio.beatsync.rendering.GameScene;
 import com.mjfelecio.beatsync.core.SceneManager;
@@ -10,51 +12,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        // UI setup
-        Canvas canvas = new Canvas(GameConfig.PLAYFIELD_WIDTH, GameConfig.PLAYFIELD_HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
+    public void start(Stage stage) {
         SceneManager.initialize(GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT, stage);
 
-        GameState.getInstance().setCurrentScene(GameScene.TITLE_SCREEN);
-
-        Scene scene = new Scene(new StackPane(canvas), GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
+        GameState.getInstance().setCurrentScene(GameScene.GAMEPLAY);
 
         stage.setTitle("Beat Sync: VSRG made with Java");
         stage.show();
-
-        // Game setup
-        gameEngine = new GameEngine();
-        try {
-            String beatmapOszFolderPath = new File("src/main/resources/com/mjfelecio/beatsync/beatmaps/1301440 TrySail - Utsuroi (Short Ver.) (another copy).osz_FILES/").toURI().toString();
-            gameEngine.initialize(beatmapOszFolderPath);
-            gameEngine.start();
-        } catch (RuntimeException e) {
-            System.err.println("Failed to start game: " + e.getMessage());
-            return;
-        }
-
-        // Input Setup
-        InputHandler inputHandler = gameEngine.getInputHandler();
-        scene.setOnKeyPressed(event ->
-                inputHandler.handleKeyPress(event.getCode(), gameEngine.getCurrentTime()));
-        scene.setOnKeyReleased(event ->
-                inputHandler.handleKeyRelease(event.getCode(), gameEngine.getCurrentTime()));
-
-        // Game Loop
-        new AnimationTimer() {
-            private long lastTime = 0;
-
-            @Override
-            public void handle(long now) {
-                long deltaTime = now - lastTime;
-                lastTime = now;
-
-                gameEngine.update(deltaTime);
-                gameEngine.render(gc);
-            }
-        }.start();
     }
 
     public static void main(String[] args) {
