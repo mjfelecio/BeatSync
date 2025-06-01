af6d3c11b99efaaeffdc1ba56abf8d59694d9ae8
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-01 20:50:09 +0800

    feat: Create separate Difficulty class for a single beatmap difficulty
    This now handles a difficulty only, along with the notes that is contained in the diff.

---
+++ src/main/java/com/mjfelecio/beatsync/object/Difficulty.java
@@ -0,0 +1,31 @@
+package com.mjfelecio.beatsync.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Represents a diff in a single beatmap such as Easy, Normal, etc.
+public class Difficulty {
+    private String title;
+    private List<Note> notes = new ArrayList<>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void addNote(Note note) {
+        this.notes.add(note);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Title: %s, Notes: %d", title, notes.size());
+    }
+}
