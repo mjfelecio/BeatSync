7035de8285b6dd8e4b3433769608562a58616caa
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-02 15:37:34 +0800

    feat: Add diffName in Beatmap class

--- src/main/java/com/mjfelecio/beatsync/object/Beatmap.java
+++ src/main/java/com/mjfelecio/beatsync/object/Beatmap.java
@@ -6,6 +6,7 @@ import java.util.List;
 // Represents a diff in a single beatmap such as Easy, Normal, etc.
 public class Beatmap {
     private String title;
+    private String diffName;
     private final List<Note> notes = new ArrayList<>();
 
     public String getTitle() {
@@ -24,8 +25,16 @@ public class Beatmap {
         this.notes.add(note);
     }
 
+    public String getDiffName() {
+        return diffName;
+    }
+
+    public void setDiffName(String diffName) {
+        this.diffName = diffName;
+    }
+
     @Override
     public String toString() {
-        return String.format("Title: %s, Notes: %d", title, notes.size());
+        return String.format("Title: %s, DiffName: %s, Notes: %d", title, diffName, notes.size());
     }
 }
