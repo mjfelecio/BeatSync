2779ce2ad1eb7de7bae6cbfce4c00fd6032be03a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-05 11:16:25 +0800

    feat: Implement AudioType to define sound effects

---
+++ src/main/java/com/mjfelecio/beatsync/audio/AudioType.java
@@ -0,0 +1,21 @@
+package com.mjfelecio.beatsync.audio;
+
+import java.util.Objects;
+
+
+public enum AudioType {
+    HIT_SOUND("/com/mjfelecio/beatsync/audio/hitsound.wav");
+
+    private final String uri;
+
+    AudioType(String resourcePath) {
+        this.uri = Objects.requireNonNull(
+                AudioType.class.getResource(resourcePath),
+                "Missing audio resource: " + resourcePath
+        ).toExternalForm();
+    }
+
+    public String getUri() {
+        return uri;
+    }
+}
