ed2cacdadcc12fd3b5f4fe8b6dfe21173900f635
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-06 21:52:14 +0800

    feat: Implement new JudgementWindows based on the new JudgementResults

--- src/main/java/com/mjfelecio/beatsync/judgement/JudgementWindow.java
+++ src/main/java/com/mjfelecio/beatsync/judgement/JudgementWindow.java
@@ -1,26 +1,22 @@
 package com.mjfelecio.beatsync.judgement;
 
 public enum JudgementWindow {
-    PERFECT(30, "Perfect"),
-    GOOD(80, "Good"),
-    MISS(150, "Miss"),
-    PERFECT_TAIL(60, "Perfect"),
-    GOOD_TAIL(160, "Good"),
-    MISS_TAIL(300, "Miss");
+    PERFECT(30),
+    GREAT(80),
+    MEH(120),
+    MISS(150),
+    PERFECT_TAIL(60),
+    GREAT_TAIL(160),
+    MEH_TAIL(240),
+    MISS_TAIL(300);
 
     private final int millis;
-    private final String description;
 
-    JudgementWindow(int millis, String description) {
+    JudgementWindow(int millis) {
         this.millis = millis;
-        this.description = description;
     }
 
     public int getMillis() {
         return this.millis;
-    }
-
-    public String getDescription() {
-        return this.description;
     }
 }
