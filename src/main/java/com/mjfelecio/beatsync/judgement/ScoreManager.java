14b230fc380b5073edb8b9532819904dd8b8be05
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-06 21:58:16 +0800

    feat: Replace "300" magic number with PERFECT_SCORE_VALUE constant

--- src/main/java/com/mjfelecio/beatsync/judgement/ScoreManager.java
+++ src/main/java/com/mjfelecio/beatsync/judgement/ScoreManager.java
@@ -8,19 +8,20 @@ public class ScoreManager {
     private final int maxScore;
     private int totalHits = 0;
     private int totalPossibleScore = 0;
+    private final int PERFECT_SCORE_VALUE = JudgementResult.PERFECT.getScore();
 
     public ScoreManager(int regularNoteCount, int holdNoteCount) {
-        this.maxScore = (regularNoteCount * 300) + (holdNoteCount * 2 * 300);
+        this.maxScore = (regularNoteCount * PERFECT_SCORE_VALUE) + (holdNoteCount * 2 * PERFECT_SCORE_VALUE);
     }
 
     public void registerJudgement(JudgementResult judgement) {
         currentScore += judgement.getScore();
-        totalPossibleScore += 300;
+        totalPossibleScore += PERFECT_SCORE_VALUE;
         totalHits++;
     }
 
     public double getAccuracy() {
-        double accuracy = totalHits == 0 ? 100.0 : (currentScore * 100.0 / (totalHits * 300));
+        double accuracy = totalHits == 0 ? 100.0 : (currentScore * 100.0 / (totalHits * PERFECT_SCORE_VALUE));
         // Round to the 2 decimal digits
         return new BigDecimal(accuracy).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
