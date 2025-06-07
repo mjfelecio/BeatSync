8309fb1e2f2bcf86a7729fa02b4b7e819ebae41a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-07 23:26:45 +0800

    feat: Reordered the layout of SongSelectController

--- src/main/java/com/mjfelecio/beatsync/controller/SongSelectController.java
+++ src/main/java/com/mjfelecio/beatsync/controller/SongSelectController.java
@@ -6,6 +6,7 @@ import com.mjfelecio.beatsync.object.BeatmapSet;
 import com.mjfelecio.beatsync.parser.BeatmapLoader;
 import com.mjfelecio.beatsync.rendering.GameScene;
 import com.mjfelecio.beatsync.state.GameState;
+import com.mjfelecio.beatsync.utils.ImageProvider;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
@@ -13,8 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 
 import java.io.File;
@@ -22,66 +22,98 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SongSelectController {
-    private ListView<BeatmapSet> songListView = new ListView<>();
-    private ListView<Beatmap> diffListView = new ListView<>();
+    private ListView<BeatmapSet> songListView;
+    private ListView<Beatmap> diffListView;
     private VBox difficultyListViewWrapper;
 
     private BeatmapSet selectedBeatmapSet;
     private Beatmap selectedBeatmap;
 
     public Scene getSongSelectScene() {
-            VBox root = new VBox();
-            root.setPrefSize(600, 800);
-            root.setAlignment(Pos.CENTER);
-            root.setStyle("-fx-padding: 20;");
-
-            Label songSelectLabel = new Label("Song Select");
-            songSelectLabel.setFont(new Font("Verdana", 25));
-
-            songListView = createSongListView();
-            songListView.setPrefWidth(400);
-
-            difficultyListViewWrapper = new VBox(5);
-            difficultyListViewWrapper.setAlignment(Pos.CENTER);
-            difficultyListViewWrapper.setPrefHeight(150);
-
-            HBox buttonContainer = new HBox(10);
-
-            Button backButton = new Button("Back");
-            backButton.setOnAction(e -> SceneManager.getInstance().setCurrentScene(GameScene.TITLE_SCREEN));
-
-            Button playButton = new Button("Play");
-            playButton.setOnAction(e -> {
-                GameState.getInstance().setCurrentBeatmap(selectedBeatmap);
-                SceneManager.getInstance().setCurrentScene(GameScene.GAMEPLAY);
-            });
-
-            buttonContainer.getChildren().addAll(backButton, playButton);
-            buttonContainer.setAlignment(Pos.CENTER);
-
-            root.getChildren().addAll(songSelectLabel, songListView, new Label("Difficulties"), difficultyListViewWrapper, buttonContainer);
-
+        VBox root = createRootLayout();
+        root.getChildren().addAll(
+                createTitleLabel(),
+                createListViewSection(),
+                createNavigationButtons()
+        );
         return new Scene(root, 1920, 1080);
     }
 
-    public ListView<BeatmapSet> createSongListView() {
+    private VBox createRootLayout() {
+        VBox root = new VBox(30);
+        root.setAlignment(Pos.CENTER);
+        root.setPrefSize(1200, 1000);
+        root.setStyle("-fx-padding: 20;");
+        root.setBackground(ImageProvider.SONG_SELECT_BG.getImageAsBackground());
+        return root;
+    }
+
+    private Label createTitleLabel() {
+        Label title = new Label("Song Select");
+        title.setFont(new Font("Verdana", 25));
+        title.setStyle("-fx-text-fill: green;");
+        return title;
+    }
+
+    private HBox createListViewSection() {
+        HBox listViewContainer = new HBox(40);
+        listViewContainer.setAlignment(Pos.TOP_CENTER);
+
+        VBox songListSection = new VBox(10, createSectionLabel("Select Song"), createSongListView());
+        songListSection.setAlignment(Pos.TOP_CENTER);
+
+        VBox difficultySection = new VBox(10);
+        difficultySection.setAlignment(Pos.TOP_CENTER);
+        difficultyListViewWrapper = new VBox(5);
+        difficultyListViewWrapper.setAlignment(Pos.CENTER);
+        difficultySection.getChildren().addAll(createSectionLabel("Select Difficulty"), difficultyListViewWrapper);
+
+        listViewContainer.getChildren().addAll(songListSection, difficultySection);
+        return listViewContainer;
+    }
+
+    private HBox createNavigationButtons() {
+        Button backButton = new Button("Back");
+        backButton.setOnAction(e -> SceneManager.getInstance().setCurrentScene(GameScene.TITLE_SCREEN));
+
+        Button playButton = new Button("Play");
+        playButton.setOnAction(e -> {
+            GameState.getInstance().setCurrentBeatmap(selectedBeatmap);
+            SceneManager.getInstance().setCurrentScene(GameScene.GAMEPLAY);
+        });
+
+        HBox buttonBox = new HBox(20, backButton, playButton);
+        buttonBox.setAlignment(Pos.CENTER);
+        return buttonBox;
+    }
+
+    private Label createSectionLabel(String text) {
+        Label label = new Label(text);
+        label.setFont(Font.font(16));
+        label.setStyle("-fx-text-fill: green;");
+        return label;
+    }
+
+    private ListView<BeatmapSet> createSongListView() {
+        songListView = new ListView<>();
         BeatmapLoader.load();
-        ObservableList<BeatmapSet> songs = FXCollections.observableArrayList(BeatmapLoader.getInstance().getAllBeatmaps());
-        songListView.setItems(songs);
+
+        ObservableList<BeatmapSet> beatmapSets = FXCollections.observableArrayList(
+                BeatmapLoader.getInstance().getAllBeatmaps()
+        );
+        songListView.setItems(beatmapSets);
 
         songListView.setCellFactory(_ -> new ListCell<>() {
-            private final HBox content;
-            private final ImageView thumbnail;
-            private final Label title;
+            private final HBox content = new HBox(10);
+            private final ImageView thumbnail = new ImageView();
+            private final Label title = new Label();
 
             {
-                thumbnail = new ImageView();
-                thumbnail.setFitWidth(50);
-                thumbnail.setFitHeight(50);
-                title = new Label();
+                thumbnail.setFitWidth(80);
+                thumbnail.setFitHeight(80);
                 title.setStyle("-fx-font-size: 16px;");
-                content = new HBox(10, thumbnail, title);
                 content.setAlignment(Pos.CENTER_LEFT);
+                content.getChildren().addAll(thumbnail, title);
             }
 
             @Override
@@ -101,28 +133,29 @@ public class SongSelectController {
         songListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
             selectedBeatmapSet = newVal;
             if (selectedBeatmapSet != null) {
-                difficultyListViewWrapper.getChildren().remove(diffListView);
-
-                diffListView = createDifficultyListView(selectedBeatmapSet);
-                difficultyListViewWrapper.getChildren().add(diffListView);
+                updateDifficultyList(selectedBeatmapSet);
             }
         });
 
         return songListView;
     }
 
-    private ListView<Beatmap> createDifficultyListView(BeatmapSet beatmapSet) {
-        ListView<Beatmap> diffListView = new ListView<>();
-        diffListView.setPrefHeight(120);
+    private void updateDifficultyList(BeatmapSet beatmapSet) {
+        difficultyListViewWrapper.getChildren().clear();
+        diffListView = createDifficultyListView(beatmapSet);
+        difficultyListViewWrapper.getChildren().add(diffListView);
+    }
 
-        // Sort difficulties by note count ascending (more notes = higher diff)
-        // This is a temporary workaround until I get to implement star rating calculation
+    private ListView<Beatmap> createDifficultyListView(BeatmapSet beatmapSet) {
         List<Beatmap> sortedDiffs = beatmapSet.getDifficulties().stream()
                 .sorted(Comparator.comparingInt(a -> a.getNotes().size()))
                 .toList();
 
+        ListView<Beatmap> diffListView = new ListView<>();
+        diffListView.setPrefHeight(150);
         diffListView.setItems(FXCollections.observableArrayList(sortedDiffs));
-        diffListView.setCellFactory(lv -> new ListCell<>() {
+
+        diffListView.setCellFactory(_ -> new ListCell<>() {
             @Override
             protected void updateItem(Beatmap diff, boolean empty) {
                 super.updateItem(diff, empty);
