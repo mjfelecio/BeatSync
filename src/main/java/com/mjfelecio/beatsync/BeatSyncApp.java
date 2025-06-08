4bb07c101f0bc75356bde27f28497b76c0cb8612
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-08 21:52:49 +0800

    feat: Move Application starter to BeatSyncApp

---
+++ src/main/java/com/mjfelecio/beatsync/BeatSyncApp.java
@@ -0,0 +1,21 @@
+package com.mjfelecio.beatsync;
+
+import com.mjfelecio.beatsync.config.GameConfig;
+import com.mjfelecio.beatsync.core.SceneManager;
+import com.mjfelecio.beatsync.rendering.GameScene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BeatSyncApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        // Add -1 as a hacky way to not shift the scenes to the top right of the screen when switching scenes
+        SceneManager.initialize(GameConfig.SCREEN_WIDTH - 1 , GameConfig.SCREEN_HEIGHT - 1, stage);
+
+        SceneManager.getInstance().setCurrentScene(GameScene.TITLE_SCREEN);
+
+        stage.setTitle("Beat Sync: VSRG made with Java");
+        stage.setMaximized(true);
+        stage.show();
+    }
+}
--- src/main/java/com/mjfelecio/beatsync/Main.java
+++ src/main/java/com/mjfelecio/beatsync/Main.java
@@ -1,26 +1,9 @@
 package com.mjfelecio.beatsync;
 
-import com.mjfelecio.beatsync.config.GameConfig;
-import com.mjfelecio.beatsync.rendering.GameScene;
-import com.mjfelecio.beatsync.core.SceneManager;
 import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        // Add -1 as a hacky way to not shift the scenes to the top right of the screen when switching scenes
-        SceneManager.initialize(GameConfig.SCREEN_WIDTH - 1 , GameConfig.SCREEN_HEIGHT - 1, stage);
-
-        SceneManager.getInstance().setCurrentScene(GameScene.TITLE_SCREEN);
-
-        stage.setTitle("Beat Sync: VSRG made with Java");
-        stage.setMaximized(true);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(BeatSyncApp.class, args);
     }
 }
