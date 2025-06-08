20949e8d59990416fe7272c3fec6a54f8918afc2
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-08 16:58:57 +0800

    feat: Set Note Approach Time as 700ms for testing

--- src/main/java/com/mjfelecio/beatsync/config/GameConfig.java
+++ src/main/java/com/mjfelecio/beatsync/config/GameConfig.java
@@ -11,7 +11,7 @@ public final class GameConfig {
     public static final int NUM_LANES = 4;
     public static final int NOTE_DIAMETER = 80;
     // Will probably swap this to a double later if I want them to control approach time
-    public static final int NOTE_APPROACH_TIME = 900;
+    public static final int NOTE_APPROACH_TIME = 700;
 
     public static final int HIT_LINE_Y = PLAYFIELD_HEIGHT - 150;
     public static final String BEATMAP_DIRECTORY = "src/main/resources/com/mjfelecio/beatsync/beatmaps";
