dbca6d20fd2abcba77a5d341260b7e410a654fc9
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-09 15:32:23 +0800

    feat: Implement SettingsManager class

---
+++ src/main/java/com/mjfelecio/beatsync/config/SettingsManager.java
@@ -0,0 +1,57 @@
+package com.mjfelecio.beatsync.config;
+
+import com.mjfelecio.beatsync.judgement.JudgementMode;
+
+public class SettingsManager {
+    private final SettingsManager instance = new SettingsManager();
+
+    private int scrollSpeed;
+    private JudgementMode judgementMode;
+    private int musicVolume;
+    private int effectsVolume;
+
+    private SettingsManager() {
+        // Default settings
+        this.scrollSpeed = 1000;
+        this.judgementMode = JudgementMode.NORMAL;
+        this.musicVolume = 100;
+        this.effectsVolume = 100;
+    }
+
+    public SettingsManager getInstance() {
+        return instance;
+    }
+
+
+    public int getScrollSpeed() {
+        return scrollSpeed;
+    }
+
+    public void setScrollSpeed(int scrollSpeed) {
+        this.scrollSpeed = scrollSpeed;
+    }
+
+    public JudgementMode getJudgementMode() {
+        return judgementMode;
+    }
+
+    public void setJudgementMode(JudgementMode judgementMode) {
+        this.judgementMode = judgementMode;
+    }
+
+    public int getMusicVolume() {
+        return musicVolume;
+    }
+
+    public void setMusicVolume(int musicVolume) {
+        this.musicVolume = musicVolume;
+    }
+
+    public int getEffectsVolume() {
+        return effectsVolume;
+    }
+
+    public void setEffectsVolume(int effectsVolume) {
+        this.effectsVolume = effectsVolume;
+    }
+}
