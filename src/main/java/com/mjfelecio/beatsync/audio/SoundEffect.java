fe3c1041a1daaa26b3a2d7179133461b08f63b8c
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-10 07:01:25 +0800

    refactor: Rename audio dir to sound-effects

--- src/main/java/com/mjfelecio/beatsync/audio/SoundEffect.java
+++ src/main/java/com/mjfelecio/beatsync/audio/SoundEffect.java
@@ -3,8 +3,8 @@ package com.mjfelecio.beatsync.audio;
 import java.util.Objects;
 
 public enum SoundEffect {
-    HITSOUND("/com/mjfelecio/beatsync/audio/hitsound.wav"),
-    HOLDBREAK("/com/mjfelecio/beatsync/audio/holdbreak.wav");
+    HITSOUND("/com/mjfelecio/beatsync/sound-effects/hitsound.wav"),
+    HOLDBREAK("/com/mjfelecio/beatsync/sound-effects/holdbreak.wav");
 
     private final String uri;
 
