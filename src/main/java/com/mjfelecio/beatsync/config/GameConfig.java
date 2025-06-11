c292467518fcbbe919dbe6c913c7884ad93b1166
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-11 21:45:20 +0800

    feat: Add ENVIRONMENT config in GameConfig
    It's becoming annoying having to change things each time I want to deploy it.

--- src/main/java/com/mjfelecio/beatsync/config/GameConfig.java
+++ src/main/java/com/mjfelecio/beatsync/config/GameConfig.java
@@ -3,6 +3,8 @@ package com.mjfelecio.beatsync.config;
 import javafx.scene.paint.Color;
 
 public final class GameConfig {
+    public static final String ENVIRONMENT = "DEV"; // OR PROD
+
     public static final int SCREEN_WIDTH = 1920;
     public static final int SCREEN_HEIGHT = 1080;
     public static final int PLAYFIELD_WIDTH = 400;
@@ -14,7 +16,7 @@ public final class GameConfig {
     public static final int AUDIO_LEAD_IN = 1000;
 
     public static final int HIT_LINE_Y = PLAYFIELD_HEIGHT - 150;
-    public static final String BEATMAP_DIRECTORY = "src/main/resources/com/mjfelecio/beatsync/beatmaps";
+    public static final String BEATMAP_DIRECTORY = ENVIRONMENT.equals("DEV") ?  "src/main/resources/com/mjfelecio/beatsync/beatmaps" : "beatmaps";
 
     // Note Colors
     public static Color INNER_NOTE_COLOR = Color.valueOf("#11B6E4");
